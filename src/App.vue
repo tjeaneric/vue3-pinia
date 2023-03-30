a4561e12fc566e9e63979b5f0a2e810a842ad1e0
tjeaneric <ericjohn415@gmail.com>
2023-03-30 11:35:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BaseContainer from './components/BaseContainer.vue'
+</script>
+
+<template>
+  <base-container title="Pinia"></base-container>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/BaseContainer.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(['title'])
+</script>
+
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+section {
+  max-width: 30rem;
+  margin: 2rem auto;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
